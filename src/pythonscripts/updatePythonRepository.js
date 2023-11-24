22be8fef030bdd296f8a2b513987e73ce5268fed
Petr Pavlín <pe.pavlin@gmail.com>
2023-11-24 11:21:41 +0100

    Use requirements.txt

--- src/pythonscripts/updatePythonRepository.js
+++ src/pythonscripts/updatePythonRepository.js
@@ -1,20 +1,28 @@
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 const path = require('path');
-const repoDir = path.join(__dirname, 'src/pythonscripts/image-parser');
 
-// Funkce pro naklonování nebo aktualizaci repozitáře
-const updateRepository = () => {
-  try {
-    // Naklonování repozitáře, pokud neexistuje
-    execSync(`git clone git@github.com:WorshipTool/image-parser.git ${repoDir}`, { stdio: 'inherit' });
+// Set the path to the repository
+const repoPath = path.resolve(__dirname, 'image-parser');
 
-    // Aktualizace repozitáře, pokud již existuje
-    execSync(`cd ${repoDir} && git pull origin master`, { stdio: 'inherit' });
-  } catch (error) {
-    console.error('Chyba při nakládání nebo aktualizaci repozitáře:', error.message);
-    process.exit(1);
+// Set the repository URL
+const repoUrl = 'https://github.com/WorshipTool/image-parser.git';
+
+// Remove the folder regardless of errors
+exec(`rimraf ${repoPath}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error while deleting folder: ${error}`);
+    return;
   }
-};
 
-// Spuštění funkce pro aktualizaci repozitáře
-updateRepository();
+  console.log(`Folder ${repoPath} has been successfully prepared for cloning...`);
+
+  // Clone the repository
+  exec(`git clone ${repoUrl} ${repoPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error while cloning the repository: ${error}`);
+      return;
+    }
+
+    console.log(`Repository has been successfully cloned to ${repoPath}.`);
+  });
+});
